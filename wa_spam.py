09f34cfd6b7ce92bd1ff0f6178c119e844f90314
Muhammad Adzani <adzanimuhammad@gmail.com>
2022-08-21 09:16:38 +0700

    Ver 2.0 | Delete lib | Comment Update

--- wa_spam.py
+++ wa_spam.py
@@ -1,12 +1,22 @@
-from turtle import pos
 import pyautogui, time
-time.sleep(2)
-position =pyautogui.position()
-pesan = "<<ENTER YOUR MESSAGES HERE>>"
 
+#TIME TO PREPARE OPEN WHATSAPP DEKSTOP or WHATSAPP WEB"
+#PLEASE BE AWARE OF THIS. CAUSED SOME SCREEN ERROR WHEN YOU OPEN ANOTHER SCREEN!
+time.sleep(5)
+
+position =pyautogui.position()
+sendMessages = "<<ENTER YOUR MESSAGES HERE>>"
+
+#START LOOPING
 for a in range(50):
     pyautogui.click(position.x,position.y)
-    pyautogui.typewrite(pesan)
-    print(pesan)
+    pyautogui.typewrite(sendMessages)
+    
+    #CHECK STATEMENT IF SUCCESS ON TERMINAL
+    print(sendMessages)
+
+    #GIVE DELAY TIME TO SEND MESSAGES EVERY SECONDS
     time.sleep(1)
+
+    #AUTO ENTER 
     pyautogui.typewrite(["enter"])
